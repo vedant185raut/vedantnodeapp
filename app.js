20bf81f98dd22620a0f36934112e040607055f10
Vedant Raut <105361526+vedant185raut@users.noreply.github.com>
2025-01-09 12:45:53 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+// app.js 
+const express = require("express"); 
+const app = express(); 
+ 
+app.use(express.static('public')); 
+ 
+const port = process.env.PORT || 3000; 
+app.listen(port, () => { 
+    console.log(`Server running at http://localhost:${port}/`); 
+}); 
